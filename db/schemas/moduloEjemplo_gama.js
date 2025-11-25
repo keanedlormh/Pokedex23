7c4a22835e61c993ab4af9096cc1d8bd78274671
keanedlormh <64150049+keanedlormh@users.noreply.github.com>
2025-11-25 11:13:02 +0100

    Add files via upload

---
+++ db/schemas/moduloEjemplo_gama.js
@@ -0,0 +1,332 @@
+/**
+ * Schema: ejemplo_gama
+ */
+
+const EJEMPLO_GAMA_SCHEMA_GROUPS = [
+    {
+        "group": "GRUPO_A",
+        "attrs": [
+            {
+                "code": "atributo_1",
+                "desc": "Atributo número 1"
+            },
+            {
+                "code": "atributo_2",
+                "desc": "Atributo número 2"
+            },
+            {
+                "code": "atributo_3",
+                "desc": "Atributo número 3"
+            },
+            {
+                "code": "atributo_4",
+                "desc": "Atributo número 4"
+            }
+        ]
+    },
+    {
+        "group": "GRUPO_B",
+        "attrs": [
+            {
+                "code": "atributo_5",
+                "desc": "Atributo número 5"
+            },
+            {
+                "code": "atributo_6",
+                "desc": "Atributo número 6"
+            },
+            {
+                "code": "atributo_7",
+                "desc": "Atributo número 7"
+            },
+            {
+                "code": "atributo_8",
+                "desc": "Atributo número 8"
+            },
+            {
+                "code": "atributo_9",
+                "desc": "Atributo número 9"
+            },
+            {
+                "code": "atributo_10",
+                "desc": "Atributo número 10"
+            }
+        ]
+    },
+    {
+        "group": "GRUPO_C",
+        "attrs": [
+            {
+                "code": "atributo_11",
+                "desc": "Atributo número 11"
+            },
+            {
+                "code": "atributo_12",
+                "desc": "Atributo número 12"
+            },
+            {
+                "code": "atributo_13",
+                "desc": "Atributo número 13"
+            },
+            {
+                "code": "atributo_14",
+                "desc": "Atributo número 14"
+            },
+            {
+                "code": "atributo_15",
+                "desc": "Atributo número 15"
+            },
+            {
+                "code": "atributo_16",
+                "desc": "Atributo número 16"
+            },
+            {
+                "code": "atributo_17",
+                "desc": "Atributo número 17"
+            },
+            {
+                "code": "atributo_18",
+                "desc": "Atributo número 18"
+            },
+            {
+                "code": "atributo_19",
+                "desc": "Atributo número 19"
+            },
+            {
+                "code": "atributo_20",
+                "desc": "Atributo número 20"
+            }
+        ]
+    },
+    {
+        "group": "GRUPO_D",
+        "attrs": [
+            {
+                "code": "atributo_21",
+                "desc": "Atributo número 21"
+            },
+            {
+                "code": "atributo_22",
+                "desc": "Atributo número 22"
+            },
+            {
+                "code": "atributo_23",
+                "desc": "Atributo número 23"
+            },
+            {
+                "code": "atributo_24",
+                "desc": "Atributo número 24"
+            },
+            {
+                "code": "atributo_25",
+                "desc": "Atributo número 25"
+            }
+        ]
+    },
+    {
+        "group": "GRUPO_E",
+        "attrs": [
+            {
+                "code": "atributo_26",
+                "desc": "Atributo número 26"
+            },
+            {
+                "code": "atributo_27",
+                "desc": "Atributo número 27"
+            },
+            {
+                "code": "atributo_28",
+                "desc": "Atributo número 28"
+            }
+        ]
+    },
+    {
+        "group": "GRUPO_F",
+        "attrs": [
+            {
+                "code": "atributo_29",
+                "desc": "Atributo número 29"
+            },
+            {
+                "code": "atributo_30",
+                "desc": "Atributo número 30"
+            },
+            {
+                "code": "atributo_31",
+                "desc": "Atributo número 31"
+            },
+            {
+                "code": "atributo_32",
+                "desc": "Atributo número 32"
+            }
+        ]
+    },
+    {
+        "group": "GRUPO_G",
+        "attrs": [
+            {
+                "code": "atributo_33",
+                "desc": "Atributo número 33"
+            },
+            {
+                "code": "atributo_34",
+                "desc": "Atributo número 34"
+            },
+            {
+                "code": "atributo_35",
+                "desc": "Atributo número 35"
+            },
+            {
+                "code": "atributo_36",
+                "desc": "Atributo número 36"
+            },
+            {
+                "code": "atributo_37",
+                "desc": "Atributo número 37"
+            }
+        ]
+    },
+    {
+        "group": "GRUPO_H",
+        "attrs": [
+            {
+                "code": "atributo_38",
+                "desc": "Atributo número 38"
+            },
+            {
+                "code": "atributo_39",
+                "desc": "Atributo número 39"
+            },
+            {
+                "code": "atributo_40",
+                "desc": "Atributo número 40"
+            }
+        ]
+    },
+    {
+        "group": "GRUPO_I",
+        "attrs": [
+            {
+                "code": "atributo_41",
+                "desc": "Atributo número 41"
+            },
+            {
+                "code": "atributo_42",
+                "desc": "Atributo número 42"
+            },
+            {
+                "code": "atributo_43",
+                "desc": "Atributo número 43"
+            },
+            {
+                "code": "atributo_44",
+                "desc": "Atributo número 44"
+            }
+        ]
+    },
+    {
+        "group": "GRUPO_J",
+        "attrs": [
+            {
+                "code": "atributo_45",
+                "desc": "Atributo número 45"
+            },
+            {
+                "code": "atributo_46",
+                "desc": "Atributo número 46"
+            },
+            {
+                "code": "atributo_47",
+                "desc": "Atributo número 47"
+            }
+        ]
+    },
+    {
+        "group": "GRUPO_K",
+        "attrs": [
+            {
+                "code": "atributo_48",
+                "desc": "Atributo número 48"
+            },
+            {
+                "code": "atributo_49",
+                "desc": "Atributo número 49"
+            },
+            {
+                "code": "atributo_50",
+                "desc": "Atributo número 50"
+            },
+            {
+                "code": "atributo_51",
+                "desc": "Atributo número 51"
+            }
+        ]
+    },
+    {
+        "group": "GRUPO_L",
+        "attrs": [
+            {
+                "code": "atributo_52",
+                "desc": "Atributo número 52"
+            },
+            {
+                "code": "atributo_53",
+                "desc": "Atributo número 53"
+            },
+            {
+                "code": "atributo_54",
+                "desc": "Atributo número 54"
+            },
+            {
+                "code": "atributo_55",
+                "desc": "Atributo número 55"
+            },
+            {
+                "code": "atributo_56",
+                "desc": "Atributo número 56"
+            }
+        ]
+    },
+    {
+        "group": "GRUPO_M",
+        "attrs": [
+            {
+                "code": "atributo_57",
+                "desc": "Atributo número 57"
+            },
+            {
+                "code": "atributo_58",
+                "desc": "Atributo número 58"
+            },
+            {
+                "code": "atributo_59",
+                "desc": "Atributo número 59"
+            },
+            {
+                "code": "atributo_60",
+                "desc": "Atributo número 60"
+            },
+            {
+                "code": "atributo_61",
+                "desc": "Atributo número 61"
+            }
+        ]
+    },
+    {
+        "group": "GRUPO_N",
+        "attrs": [
+            {
+                "code": "atributo_62",
+                "desc": "Atributo número 62"
+            },
+            {
+                "code": "atributo_63",
+                "desc": "Atributo número 63"
+            }
+        ]
+    }
+];
+
+if (window.APP_DB && typeof window.APP_DB.registerSchema === 'function') {
+    window.APP_DB.registerSchema('ejemplo_gama', EJEMPLO_GAMA_SCHEMA_GROUPS);
+}
